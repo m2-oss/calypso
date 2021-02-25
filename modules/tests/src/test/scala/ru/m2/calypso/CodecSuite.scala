c2bb65d72ac1361418fcc6ecc068f0ec7ecde0e3
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2021-02-25 12:34:12 +0300

    Update tests, add law testing docs (#20)

---
+++ modules/tests/src/test/scala/ru/m2/calypso/CodecSuite.scala
@@ -0,0 +1,38 @@
+package ru.m2.calypso
+
+import cats.data.NonEmptyList
+import cats.laws.discipline.arbitrary._
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.collection.NonEmpty
+import eu.timepit.refined.scalacheck.string._
+import eu.timepit.refined.string.Uuid
+import org.scalatest.funsuite.AnyFunSuiteLike
+import org.typelevel.discipline.scalatest.Discipline
+import ru.m2.calypso.MissingInstances._
+import ru.m2.calypso.testing.CodecTests
+
+import java.time.Instant
+import java.util.UUID
+import scala.collection.immutable.{SortedMap, SortedSet}
+
+class CodecSuite extends AnyFunSuiteLike with Discipline {
+  checkAll("Codec[Unit]", CodecTests[Unit].codec)
+  checkAll("Codec[Boolean]", CodecTests[Boolean].codec)
+  checkAll("Codec[Int]", CodecTests[Int].codec)
+  checkAll("Codec[Long]", CodecTests[Long].codec)
+  checkAll("Codec[String]", CodecTests[String].codec)
+  checkAll("Codec[(Int, Long)]", CodecTests[(Int, Long)].codec)
+  checkAll("Codec[NonEmptyList[Int]]", CodecTests[NonEmptyList[Int]].codec)
+  checkAll("Codec[List[Int]]", CodecTests[List[Int]].codec)
+  checkAll("Codec[Set[Int]]", CodecTests[Set[Int]].codec)
+  checkAll("Codec[SortedSet[Int]]", CodecTests[SortedSet[Int]].codec)
+  checkAll("Codec[Map[Int, Long]]", CodecTests[Map[Int, Long]].codec)
+  checkAll("Codec[SortedMap[Int, Long]]", CodecTests[SortedMap[Int, Long]].codec)
+  checkAll("Codec[Option[Int]]", CodecTests[Option[Int]].codec)
+  checkAll("Codec[Either[String, Int]]", CodecTests[Either[String, Int]].codec)
+  checkAll("Codec[Array[Byte]]", CodecTests[Array[Byte]].codec)
+  checkAll("Codec[Instant]", CodecTests[Instant].codec)
+  checkAll("Codec[UUID]", CodecTests[UUID].codec)
+  checkAll("Codec[String Refined NonEmpty]", CodecTests[String Refined NonEmpty].codec)
+  checkAll("Codec[String Refined Uuid]", CodecTests[String Refined Uuid].codec)
+}
--- modules/tests/src/test/scala/ru/m2/calypso/KeyCodecSuite.scala
+++ modules/tests/src/test/scala/ru/m2/calypso/KeyCodecSuite.scala
@@ -1,4 +1,5 @@
 package ru.m2.calypso
+
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.collection.NonEmpty
 import eu.timepit.refined.scalacheck.string._
@@ -7,8 +8,8 @@ import org.typelevel.discipline.scalatest.Discipline
 import ru.m2.calypso.MissingInstances._
 import ru.m2.calypso.testing.KeyCodecTests
 
-class KeyCodecTestsSpec extends AnyFunSuiteLike with Discipline {
-  checkAll("KeyCodecTests[String]", KeyCodecTests[String].codec)
-  checkAll("KeyCodecTests[Int]", KeyCodecTests[Int].codec)
-  checkAll("KeyCodecTests[String Refined NonEmpty]", KeyCodecTests[String Refined NonEmpty].codec)
+class KeyCodecSuite extends AnyFunSuiteLike with Discipline {
+  checkAll("KeyCodec[String]", KeyCodecTests[String].codec)
+  checkAll("KeyCodec[Int]", KeyCodecTests[Int].codec)
+  checkAll("KeyCodec[String Refined NonEmpty]", KeyCodecTests[String Refined NonEmpty].codec)
 }
