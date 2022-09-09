1ce05d2ee24d2ea41084afb1d58cea9ffd3c769c
Luis Miguel Mejía Suárez <luis.mejias@perficient.com>
2022-09-09 14:39:43 -0500

    Add the ProductDecoders boilerplate generator

--- project/Boilerplate.scala
+++ project/Boilerplate.scala
@@ -15,6 +15,36 @@ object Boilerplate {
     def generateFileContents(): List[String]
   }
 
+  private final case object ProductDecoders extends BoilerplateFile {
+    override final val filename: String = "ProductDecoders"
+
+    override def generateFileContents(): List[String] = {
+      def generateProductDecoder(n: Int): List[String] = {
+        val parameters = letters.take(n)
+
+
+        s"final def forProduct${n}[A, ${parameters.map(l => s"${l}: Decoder").mkString(", ")}]" ::
+        s"(${parameters.map(l => s"n${l.toLower}: String").mkString(", ")})" ::
+        s"(fun: (${parameters.mkString(", ")}) => A): Decoder[A] =" ::
+        "Decoder.instance { bson =>" ::
+        "for {" ::
+        "doc <- bson.focus" ::
+        parameters.map(l => s"${l.toLower} <- doc.downField(n${l.toLower}).as[${l}]") :::
+        s"} yield fun(${parameters.map(_.toLower).mkString(", ")})" ::
+        "}" ::
+        Nil
+      }
+
+      "package ru.m2.calypso.boilerplate" ::
+      "import ru.m2.calypso.Decoder" ::
+      "import ru.m2.calypso.syntax._" ::
+      "trait ProductDecoders {" ::
+      List.tabulate(generateUpTo)(n => generateProductDecoder(n + 1)).flatten :::
+      "}" ::
+      Nil
+    }
+  }
+
   val generatorTask = Def.task {
     val log = streams.value.log
 
@@ -26,7 +56,9 @@ object Boilerplate {
       ) { _ =>
         log.info("Generating boilerplate files")
 
-        Set.empty[ProductDecoders].map { boilerplateFile =>
+        Set(
+          ProductDecoders
+        ).map { boilerplateFile =>
           // Save the boilerplate file to the managed sources dir.
           val file = (Compile / sourceManaged).value / "boilerplate" / s"${boilerplateFile.filename}.scala"
           log.log(Level.Info, s"Generating file ${file}")
