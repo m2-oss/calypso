d4be4c637c360a04919f2d71f480827141c8d491
Scala Steward <scala_steward@virtuslab.com>
2023-07-28 18:54:15 +0000

    Reformat with scalafmt 3.7.11
    Executed command: scalafmt --non-interactive

--- project/Boilerplate.scala
+++ project/Boilerplate.scala
@@ -19,24 +19,24 @@ object Boilerplate {
         val parameters = letters.take(n)
 
         s"final def forProduct${n}[A, ${parameters.map(l => s"${l}: Decoder").mkString(", ")}]" ::
-        s"(${parameters.map(l => s"n${l.toLower}: String").mkString(", ")})" ::
-        s"(fun: (${parameters.mkString(", ")}) => A): Decoder[A] =" ::
-        "Decoder.instance { bson =>" ::
-        "for {" ::
-        "doc <- bson.focus" ::
-        parameters.map(l => s"${l.toLower} <- doc.downField(n${l.toLower}).as[${l}]") :::
-        s"} yield fun(${parameters.map(_.toLower).mkString(", ")})" ::
-        "}" ::
-        Nil
+          s"(${parameters.map(l => s"n${l.toLower}: String").mkString(", ")})" ::
+          s"(fun: (${parameters.mkString(", ")}) => A): Decoder[A] =" ::
+          "Decoder.instance { bson =>" ::
+          "for {" ::
+          "doc <- bson.focus" ::
+          parameters.map(l => s"${l.toLower} <- doc.downField(n${l.toLower}).as[${l}]") :::
+          s"} yield fun(${parameters.map(_.toLower).mkString(", ")})" ::
+          "}" ::
+          Nil
       }
 
       "package ru.m2.calypso.boilerplate" ::
-      "import ru.m2.calypso.Decoder" ::
-      "import ru.m2.calypso.syntax._" ::
-      "trait ProductDecoders {" ::
-      List.tabulate(generateUpTo)(n => generateProductDecoder(n + 1)).flatten :::
-      "}" ::
-      Nil
+        "import ru.m2.calypso.Decoder" ::
+        "import ru.m2.calypso.syntax._" ::
+        "trait ProductDecoders {" ::
+        List.tabulate(generateUpTo)(n => generateProductDecoder(n + 1)).flatten :::
+        "}" ::
+        Nil
     }
   }
 
@@ -46,27 +46,29 @@ object Boilerplate {
         val parameters = letters.take(n)
 
         s"final def forCoproduct${n}[A, ${parameters.map(l => s"${l} <: A : Decoder").mkString(", ")}]" ::
-        s"(${parameters.map(l => s"t${l.toLower}: String").mkString(", ")}): Decoder[A] =" ::
-        "Decoder.instance { bson =>" ::
-        "for {" ::
-        "doc <- bson.focus" ::
-        "tag <- doc.downField(\"tag\").as[String]" ::
-        "a <- tag match {" ::
-        parameters.map(l => s"case t if t == t${l.toLower} => doc.downField(${'"'}value${'"'}).as[${l}]") :::
-        "case _ => Left(s\"Tag.unknown: [$tag]\")" ::
-        "}" ::
-        "} yield a" ::
-        "}" ::
-        Nil
+          s"(${parameters.map(l => s"t${l.toLower}: String").mkString(", ")}): Decoder[A] =" ::
+          "Decoder.instance { bson =>" ::
+          "for {" ::
+          "doc <- bson.focus" ::
+          "tag <- doc.downField(\"tag\").as[String]" ::
+          "a <- tag match {" ::
+          parameters.map(l =>
+            s"case t if t == t${l.toLower} => doc.downField(${'"'}value${'"'}).as[${l}]"
+          ) :::
+          "case _ => Left(s\"Tag.unknown: [$tag]\")" ::
+          "}" ::
+          "} yield a" ::
+          "}" ::
+          Nil
       }
 
       "package ru.m2.calypso.boilerplate" ::
-      "import ru.m2.calypso.Decoder" ::
-      "import ru.m2.calypso.syntax._" ::
-      "trait CoproductDecoders {" ::
-      List.tabulate(generateUpTo)(n => generateCoproductDecoder(n + 1)).flatten :::
-      "}" ::
-      Nil
+        "import ru.m2.calypso.Decoder" ::
+        "import ru.m2.calypso.syntax._" ::
+        "trait CoproductDecoders {" ::
+        List.tabulate(generateUpTo)(n => generateCoproductDecoder(n + 1)).flatten :::
+        "}" ::
+        Nil
     }
   }
 
@@ -76,24 +78,24 @@ object Boilerplate {
         val parameters = letters.take(n)
 
         s"final def forProduct${n}[A, ${parameters.map(l => s"${l} : Encoder").mkString(", ")}]" ::
-        s"(${parameters.map(l => s"n${l.toLower}: String").mkString(", ")})" ::
-        s"(fun: A => (${parameters.mkString(", ")})): Encoder[A] =" ::
-        "Encoder.instance { a =>" ::
-        s"val (${parameters.map(_.toLower).mkString(", ")}) = fun(a)" ::
-        "Bson.obj(" ::
-        parameters.map(l => s"n${l.toLower} -> ${l.toLower}.asBson,") :::
-        ")" ::
-        "}" ::
-        Nil
+          s"(${parameters.map(l => s"n${l.toLower}: String").mkString(", ")})" ::
+          s"(fun: A => (${parameters.mkString(", ")})): Encoder[A] =" ::
+          "Encoder.instance { a =>" ::
+          s"val (${parameters.map(_.toLower).mkString(", ")}) = fun(a)" ::
+          "Bson.obj(" ::
+          parameters.map(l => s"n${l.toLower} -> ${l.toLower}.asBson,") :::
+          ")" ::
+          "}" ::
+          Nil
       }
 
       "package ru.m2.calypso.boilerplate" ::
-      "import ru.m2.calypso.{Bson, Encoder}" ::
-      "import ru.m2.calypso.syntax._" ::
-      "trait ProductEncoders {" ::
-      List.tabulate(generateUpTo)(n => generateCoproductDecoder(n + 1)).flatten :::
-      "}" ::
-      Nil
+        "import ru.m2.calypso.{Bson, Encoder}" ::
+        "import ru.m2.calypso.syntax._" ::
+        "trait ProductEncoders {" ::
+        List.tabulate(generateUpTo)(n => generateCoproductDecoder(n + 1)).flatten :::
+        "}" ::
+        Nil
     }
   }
 
