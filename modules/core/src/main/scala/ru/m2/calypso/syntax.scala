c6ee540cb591b8c4f170158937f3d3d30e8031ad
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2023-11-28 13:18:03 +0300

    Cleanup

--- modules/core/src/main/scala/ru/m2/calypso/syntax.scala
+++ modules/core/src/main/scala/ru/m2/calypso/syntax.scala
@@ -11,7 +11,7 @@ object syntax:
     def asBson(using Encoder[A]): BsonValue = Encoder[A].apply(a)
     def asKey(using KeyEncoder[A]): String  = KeyEncoder[A].apply(a)
 
-  implicit class BsonOps(bson: BsonValue) extends AnyVal:
+  implicit class BsonValueOps(bson: BsonValue) extends AnyVal:
     def focus: Either[String, BsonDocument] =
       Either.catchNonFatal(bson.asDocument()).leftMap(_.getMessage)
 
