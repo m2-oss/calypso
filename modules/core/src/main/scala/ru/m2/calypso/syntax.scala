d528ff00c6c7528cc384900f079624f82344374a
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2023-11-28 13:33:32 +0300

    Revert "Cleanup"
    This reverts commit 63aef99c8c5f8e8e3be1b70a4fdd5ff075327abe.

--- modules/core/src/main/scala/ru/m2/calypso/syntax.scala
+++ modules/core/src/main/scala/ru/m2/calypso/syntax.scala
@@ -7,21 +7,21 @@ import scala.jdk.CollectionConverters.*
 
 object syntax:
 
-  implicit class EncoderOps[A](val a: A) extends AnyVal:
+  implicit class EncoderOps[A](a: A) extends AnyVal:
     def asBson(using Encoder[A]): BsonValue = Encoder[A].apply(a)
     def asKey(using KeyEncoder[A]): String  = KeyEncoder[A].apply(a)
 
-  implicit class BsonValueOps(val bson: BsonValue) extends AnyVal:
+  implicit class BsonValueOps(bson: BsonValue) extends AnyVal:
     def focus: Either[String, BsonDocument] =
       Either.catchNonFatal(bson.asDocument()).leftMap(_.getMessage)
 
     def as[A: Decoder]: Either[String, A] =
       Decoder[A].apply(bson)
 
-  implicit class StringOps(val s: String) extends AnyVal:
+  implicit class StringOps(s: String) extends AnyVal:
     def as[K: KeyDecoder]: Either[String, K] = KeyDecoder[K].apply(s)
 
-  implicit class BsonDocumentOps(val bson: BsonDocument) extends AnyVal:
+  implicit class BsonDocumentOps(bson: BsonDocument) extends AnyVal:
 
     /** BsonNull on non-existing keys allows to avoid Zipper/Cursor on decoding
       */
