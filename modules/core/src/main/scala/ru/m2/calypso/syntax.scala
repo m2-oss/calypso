0f3e83b940145cfc9e8f15237b7a8412c35c0514
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2023-11-28 13:16:10 +0300

    Use extension methods compatible with Scala 2.13

--- modules/core/src/main/scala/ru/m2/calypso/syntax.scala
+++ modules/core/src/main/scala/ru/m2/calypso/syntax.scala
@@ -7,27 +7,22 @@ import scala.jdk.CollectionConverters.*
 
 object syntax:
 
-  extension [A](a: A)
+  implicit class EncoderOps[A](a: A) extends AnyVal:
+    def asBson(using Encoder[A]): BsonValue = Encoder[A].apply(a)
+    def asKey(using KeyEncoder[A]): String  = KeyEncoder[A].apply(a)
 
-    def asBson(using Encoder[A]): BsonValue =
-      Encoder[A].apply(a)
-
-    def asKey(using KeyEncoder[A]): String =
-      KeyEncoder[A].apply(a)
-
-  extension (bson: BsonValue)
-
+  implicit class BsonOps(bson: BsonValue) extends AnyVal:
     def focus: Either[String, BsonDocument] =
       Either.catchNonFatal(bson.asDocument()).leftMap(_.getMessage)
 
     def as[A: Decoder]: Either[String, A] =
       Decoder[A].apply(bson)
 
-  extension (s: String)
-    def as[K: KeyDecoder]: Either[String, K] =
-      KeyDecoder[K].apply(s)
+  implicit class StringOps(s: String) extends AnyVal:
+    def as[K: KeyDecoder]: Either[String, K] = KeyDecoder[K].apply(s)
 
-  extension (bson: BsonDocument)
+  implicit class BsonDocumentOps(bson: BsonDocument) extends AnyVal:
+
     /** BsonNull on non-existing keys allows to avoid Zipper/Cursor on decoding
       */
     def downField(k: String): BsonValue =
